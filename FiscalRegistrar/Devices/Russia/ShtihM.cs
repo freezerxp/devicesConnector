7f23f826cb69ff8cf4dd3ee42c941e34d3921f74
Freezer <mail@pavshukov.ru>
2022-04-22 01:39:15 +0300

    доработка основных методов Штрих-М

--- FiscalRegistrar/Devices/Russia/ShtihM.cs
+++ FiscalRegistrar/Devices/Russia/ShtihM.cs
@@ -2,6 +2,7 @@
 using System.Text.Json;
 using devicesConnector.Common;
 using devicesConnector.Configs;
+using devicesConnector.FiscalRegistrar.Helpers;
 using devicesConnector.FiscalRegistrar.Objects;
 using devicesConnector.Helpers;
 using Enums = devicesConnector.FiscalRegistrar.Objects.Enums;
@@ -12,6 +13,7 @@ public class ShtihM : IFiscalRegistrarDevice
 {
     private Device _device;
     private dynamic? _driver;
+    private ReceiptData _receipt;
 
 
     public ShtihM(Device device)
@@ -208,27 +210,261 @@ public class ShtihM : IFiscalRegistrarDevice
 
     public void OpenReceipt(ReceiptData? receipt)
     {
-        throw new NotImplementedException();
+
+        _receipt = receipt;
+        
+
+        Payments = new Dictionary<int, decimal>();
+      //  CheckDiscountsList = new List<CheckDiscount>();
+
+        for (var i = 1; i < 17; i++)
+        {
+            Payments.Add(i, 0);
+        }
+
+        _driver.CheckType = receipt.OperationType switch
+        {
+            Enums.ReceiptOperationTypes.Sale => 0, // продажа
+            Enums.ReceiptOperationTypes.Buy => 1, // покупка
+            Enums.ReceiptOperationTypes.ReturnSale => 2, // возврат продажи
+            Enums.ReceiptOperationTypes.ReturnBuy => 3, // возврат покупки
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        CheckResult(_driver.OpenCheck());
+
+        //todo: отключение печати чека
+        //if (new ConfigsRepository<Core.Config.Devices>().Get().CheckPrinter.FiscalKkm.IsAlwaysNoPrintCheck)
+        //{
+        //    OffPrintDoc();
+        //}
+
+        var ffdV = _device.DeviceSpecificConfig.Deserialize<Objects.CountrySpecificData.Russia.KkmConfig>()
+            ?.FfdVersion;
+
+        if (ffdV != Enums.FFdVersions.Offline)
+        {
+            SetCashierData(receipt.Cashier);
+        }
+
+        if (ffdV == Enums.FFdVersions.Ffd120)
+        {
+            foreach (var item in receipt.Items)
+            {
+                var ruData = item.CountrySpecificData.Deserialize<Objects.CountrySpecificData.Russia.ReceiptItemData>();
+
+                if (ruData?.MarkingInfo == null || ruData.MarkingInfo.RawCode .IsNullOrEmpty())
+                {
+                    continue;
+                }
+
+             
+
+                //_driver.BarCode = ruData.MarkingInfo.RawCode; //должно быть с заменой?
+                _driver.BarCode = RuKkmHelper.PrepareMarkCodeForFfd120(ruData.MarkingInfo.RawCode);
+                _driver.ItemStatus = RuKkmHelper.GetMarkingCodeStatus(item, receipt.OperationType);
+                _driver.CheckItemMode = 0;
+
+                _driver.TLVDataHex = "";
+                _driver.TagNumber = 2108;
+                _driver.TagType = (int)ruData.FfdData.Unit; // тип ttByte
+                _driver.TagValueInt = 0;
+                _driver.GetTagAsTLV();
+
+
+
+                _driver.TLVDataHex = _driver.TLVDataHex;//bug: а это для чего??
+                CheckResult(_driver.FNCheckItemBarcode());
+                CheckResult(_driver.FNAcceptMarkingCode());
+            }
+        }
     }
 
     public void CloseReceipt()
     {
-        throw new NotImplementedException();
+        if (_receipt == null)
+        {
+            throw new ArgumentNullException(nameof(_receipt));
+        }
+
+        //todo: устанавливаю скидки по чеку
+        //RegisterAllCheckDiscount();
+
+
+        //RegisterAllPayments();
+
+        for (int i = 1; i < 17; i++)
+        {
+            _driver[$"Summ{i}"] = Payments[i]; //должно сработать
+        }
+
+        var ffdV = _device.DeviceSpecificConfig.Deserialize<Objects.CountrySpecificData.Russia.KkmConfig>()
+            ?.FfdVersion;
+
+        if (ffdV != Enums.FFdVersions.Offline)
+        {
+            _driver.TaxType = _receipt.CountrySpecificData.Deserialize<Objects.CountrySpecificData.Russia.ReceiptData>()
+                .TaxVariantIndex;
+
+
+            CheckResult(_driver.FNCloseCheckEx());
+        }
+        else
+        {
+            CheckResult(_driver.CloseCheck());
+        }
     }
 
     public void CancelReceipt()
     {
-        throw new NotImplementedException();
+        _receipt = null;
+        CheckResult(_driver.CancelCheck());
     }
 
     public void RegisterItem(ReceiptItem item)
     {
-        throw new NotImplementedException();
+        var goodName = item.Name;
+
+        if (goodName.Length > 100)
+        {
+            goodName = goodName.Substring(0, 100);
+        }
+
+        _driver.StringForPrinting = goodName;
+        _driver.Price = item.Price;
+        _driver.Quantity = item.Quantity;
+        _driver.Department = item.DepartmentIndex;
+
+
+
+        var ffdV = _device.DeviceSpecificConfig.Deserialize<Objects.CountrySpecificData.Russia.KkmConfig>()
+            ?.FfdVersion;
+
+
+        switch (ffdV)
+        {
+            case Enums.FFdVersions.Offline:
+            case Enums.FFdVersions.Ffd100:
+            {
+                RegisterGood_OfflineFFD10(item, _receipt.OperationType);
+                break;
+            }
+
+            case Enums.FFdVersions.Ffd105:
+            case Enums.FFdVersions.Ffd110:
+            case Enums.FFdVersions.Ffd120:
+
+
+                RegisterGood_onlineKkm(item, _receipt.OperationType);
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        //todo: печать нефискальных строк
+       // PrintNonFiscalStrings(good.CommentForFiscalCheck.Select(x => new NonFiscalString(x)).ToList());
+        _driver.StringForPrinting = "";
+    }
+
+
+    private void RegisterGood_onlineKkm(ReceiptItem good, Enums.ReceiptOperationTypes receiptType)
+    {
+        if (good == null)
+        {
+            throw new ArgumentNullException(nameof(good));
+        }
+
+        _driver.CheckType = receiptType switch
+        {
+            Enums.ReceiptOperationTypes.Sale => 1,
+            Enums.ReceiptOperationTypes.ReturnSale => 2,
+            _ => throw new NotImplementedException()
+        };
+
+
+
+        var ruData = good.CountrySpecificData.Deserialize<Objects.CountrySpecificData.Russia.ReceiptItemData>();
+
+        if (ruData?.FfdData == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        _driver.Tax1 = good.TaxRateIndex;
+        _driver.PaymentItemSign = ruData.FfdData.Subject;
+        _driver.PaymentTypeSign = ruData.FfdData.Method;
+        _driver.Summ1Enabled = false;
+
+        CheckResult(_driver.FNOperation());
+
+
+        if (ruData.MarkingInfo != null && ruData.MarkingInfo.RawCode.IsNullOrEmpty() == false)
+        {
+            var ffdV = _device.DeviceSpecificConfig.Deserialize<Objects.CountrySpecificData.Russia.KkmConfig>()
+                ?.FfdVersion;
+
+            if (ffdV == Enums.FFdVersions.Ffd120)
+            {
+               
+
+                _driver.BarCode =RuKkmHelper.PrepareMarkCodeForFfd120(ruData.MarkingInfo.RawCode);
+                CheckResult(_driver.FNSendItemBarcode());
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+      
+
+
+
+    }
+
+    private void RegisterGood_OfflineFFD10(ReceiptItem good, Enums.ReceiptOperationTypes checkType)
+    {
+        switch (checkType)
+        {
+            case Enums.ReceiptOperationTypes.Sale:
+            {
+                CheckResult(_driver.Sale());
+                break;
+            }
+            case Enums.ReceiptOperationTypes.ReturnSale:
+            {
+                CheckResult(_driver.ReturnSale());
+                break;
+            }
+            default:
+                throw new NotImplementedException();
+        }
+
+        //скидка на весь чек
+        //if (dc.CheckPrinter.FiscalKkm.FfdVersion != GlobalDictionaries.Devices.FfdVersions.OfflineKkm)
+        //{
+        //    return;
+        //}
+
+        //if (good.DiscountSum > 0)
+        //{
+        //    ShtrihDriver.StringForPrinting = @" ";
+        //    ShtrihDriver.Summ1 = good.DiscountSum;
+        //    CheckResult(ShtrihDriver.Discount());
+        //}
     }
 
     public void RegisterPayment(ReceiptPayment payment)
     {
-        throw new NotImplementedException();
+
+        if (payment.MethodIndex is < 1 or > 16)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        Payments[payment.MethodIndex] += payment.Sum;
+
     }
 
     public void PrintText(string text)
@@ -303,23 +539,56 @@ public class ShtihM : IFiscalRegistrarDevice
 
     public void CashIn(decimal sum, Cashier cashier)
     {
-        throw new NotImplementedException();
+        _driver.Summ1 = sum;
+        CheckResult(_driver.CashIncome());
     }
 
     public void CashOut(decimal sum, Cashier cashier)
     {
-        throw new NotImplementedException();
+        _driver.Summ1 = sum;
+        CheckResult(_driver.CashOutcome());
     }
 
     public void CutPaper()
     {
-        throw new NotImplementedException();
+        WaitPrint();
+
+        _driver.CutType = true;
+        var r = _driver.CutCheck();
+
+        CheckResult(r);
     }
 
     public void OpenCashBox()
     {
         throw new NotImplementedException();
     }
+
+    private void RegisterAllPayments()
+    {
+     //возможно, не нужно
+     //в методе закрытия чека переписано
+
+
+
+        _driver.Summ1 = Payments[1];
+        _driver.Summ2 = Payments[2];
+        _driver.Summ3 = Payments[3];
+        _driver.Summ4 = Payments[4];
+        _driver.Summ5 = Payments[5];
+        _driver.Summ6 = Payments[6];
+        _driver.Summ7 = Payments[7];
+        _driver.Summ8 = Payments[8];
+        _driver.Summ9 = Payments[9];
+        _driver.Summ10 = Payments[10];
+        _driver.Summ11 = Payments[11];
+        _driver.Summ12 = Payments[12];
+        _driver.Summ13 = Payments[13];
+        _driver.Summ14 = Payments[14];
+        _driver.Summ15 = Payments[15];
+        _driver.Summ16 = Payments[16];
+    }
+
 
     private Enums.KkmStatuses GetKkmState()
     {
